b88250be41afe3915c0c65c9e8c2669c46af2922
SiddhantGupta988 <siddhantsipoliya91@gmail.com>
2021-05-02 00:24:36 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+add = document.getElementById("add");
+if(add){
+add.addEventListener("click",()=>{
+    console.log("fgdfg");
+   /* title = document.getElementById('title').value;
+    desc = document.getElementById('description').value;
+    if(localStorage.getItem('itemsJson')==null)
+    {
+        itemJsonArray =[];
+        itemJsonArray.push([title,desc]);
+        localStorage.setItem('itemsJson',JSON.stringify(itemJsonArray))
+    }*/
+
+});
+}
